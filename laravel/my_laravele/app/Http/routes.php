ac3190656dfa0c670430abe3dff39f63ff495152
liangchengcheng <1038127753@qq.com>
2016-03-01 15:51:45 +0800

    php基础知识
    php基础知识

---
+++ laravel/my_laravele/app/Http/routes.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
+
+Route::get('test',function(){
+    return 'get请求';
+});
+
+Route::any('test',function(){
+    return 'get post 请求';
+});
+
+//Route::post('test',function(){
+//    return 'post请求';
+//});
