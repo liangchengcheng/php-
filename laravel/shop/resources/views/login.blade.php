e218167732b31e49fd9eb2ef30907c25ca628432
liangchengcheng <1038127753@qq.com>
2016-03-04 08:46:23 +0800

    master model
    master model

---
+++ laravel/shop/resources/views/login.blade.php
@@ -0,0 +1,13 @@
+@extends('master');
+
+@section('title','登录')
+
+@section('content')
+    <p> THIS IS LOGIN PAGE</p>
+@endsection
+
+@section('my-js')
+    <script type="text/javascript">
+        alert('LOGIN')
+    </script>
+@endsection
