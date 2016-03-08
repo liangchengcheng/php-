7f669c4b729fadc74f4736968a465c1c8abe36bf
liangchengcheng <1038127753@qq.com>
2016-03-09 07:29:24 +0800

    路由组
    路由组

--- laravel/shop/app/Http/routes.php
+++ laravel/shop/app/Http/routes.php
@@ -33,7 +33,17 @@ Route::get('/login', 'View\MemberController@toLogin');
 
 Route::get('/register', 'View\MemberController@toRegister');
 
-Route::any('service/validate_code/create', 'Service\ValidateController@create');
-Route::any('service/validate_phone/send', 'Service\ValidateController@sendSMS');
-Route::any('service/validate_email', 'Service\ValidateController@validateEmail');
-Route::post('service/register', 'Service\MemberController@register');
+//Route::any('service/validate_code/create', 'Service\ValidateController@create');
+//Route::any('service/validate_phone/send', 'Service\ValidateController@sendSMS');
+//Route::any('service/validate_email', 'Service\ValidateController@validateEmail');
+//Route::post('service/register', 'Service\MemberController@register');
+//Route::post('service/login', 'Service\MemberController@login');
+
+//路由组的概念
+Route::group(['prefix'=>'service'],function(){
+    Route::any('validate_code/create', 'Service\ValidateController@create');
+    Route::any('validate_phone/send', 'Service\ValidateController@sendSMS');
+    Route::any('validate_email', 'Service\ValidateController@validateEmail');
+    Route::post('register', 'Service\MemberController@register');
+    Route::post('login', 'Service\MemberController@login');
+});
