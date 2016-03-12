51460f7a3e322170fabce76981877e507ab87cca
liangchengcheng <1038127753@qq.com>
2016-03-12 23:31:59 +0800

    content
    content

---
+++ laravel/shop/app/Http/Controllers/Service/BookController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Service;
+
+use App\Category;
+use App\Http\Controllers\Controller;
+
+use App\Models\M3Result;
+use Illuminate\Http\Request;
+use Mail;
+
+class BookController extends Controller
+{
+  public function getCategoryByParentId($parent_id)
+  {
+    $category=Category::where('parent_id',$parent_id)->get();
+
+    $m3_result=new M3Result;
+    $m3_result->status=0;
+    $m3_result->message='返回成功';
+    $m3_result->categorys=$category;
+
+    return $m3_result->toJson();
+  }
+}
--- laravel/shop/resources/views/master.blade.php
+++ laravel/shop/resources/views/master.blade.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
-  <title>@yield('title')</title>
-  <link rel="stylesheet" href="/css/weui.css">
-  <link rel="stylesheet" href="/css/book.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="/css/weui.css">
+    <link rel="stylesheet" href="/css/book.css">
 </head>
 <body>
+<div class="bk_title_bar">
+    <img class="bk_back" src="/images/back.png" alt="" onclick="history.go(-1);">
+
+    <p class="bk_title_content"></p>
+    <img class="bk_menu" src="/images/menu.png" alt="" onclick="onMenuClick();">
+</div>
 
 <div class="page">
-  @yield('content')
+    @yield('content')
 </div>
 
 <!-- tooltips -->
 <div class="bk_toptips"><span></span></div>
 
 <div id="global_menu" onclick="onMenuClick();">
-  <div></div>
+    <div></div>
 </div>
 
 <!--BEGIN actionSheet-->
@@ -39,45 +45,49 @@
 </body>
 <script src="/js/jquery-1.11.2.min.js"></script>
 <script type="text/javascript">
-function hideActionSheet(weuiActionsheet, mask) {
-    weuiActionsheet.removeClass('weui_actionsheet_toggle');
-    mask.removeClass('weui_fade_toggle');
-    weuiActionsheet.on('transitionend', function () {
-        mask.hide();
-    }).on('webkitTransitionEnd', function () {
-        mask.hide();
-    })
-}
+    function hideActionSheet(weuiActionsheet, mask) {
+        weuiActionsheet.removeClass('weui_actionsheet_toggle');
+        mask.removeClass('weui_fade_toggle');
+        weuiActionsheet.on('transitionend', function () {
+            mask.hide();
+        }).on('webkitTransitionEnd', function () {
+            mask.hide();
+        })
+    }
+    function onMenuClick() {
+        var mask = $('#mask');
+        var weuiActionsheet = $('#weui_actionsheet');
+        weuiActionsheet.addClass('weui_actionsheet_toggle');
+        mask.show().addClass('weui_fade_toggle').click(function () {
+            hideActionSheet(weuiActionsheet, mask);
+        });
+        $('#actionsheet_cancel').click(function () {
+            hideActionSheet(weuiActionsheet, mask);
+        });
+        weuiActionsheet.unbind('transitionend').unbind('webkitTransitionEnd');
+    }
 
-function onMenuClick () {
-    var mask = $('#mask');
-    var weuiActionsheet = $('#weui_actionsheet');
-    weuiActionsheet.addClass('weui_actionsheet_toggle');
-    mask.show().addClass('weui_fade_toggle').click(function () {
+    function onMenuItemClick(index) {
+        var mask = $('#mask');
+        var weuiActionsheet = $('#weui_actionsheet');
         hideActionSheet(weuiActionsheet, mask);
-    });
-    $('#actionsheet_cancel').click(function () {
-        hideActionSheet(weuiActionsheet, mask);
-    });
-    weuiActionsheet.unbind('transitionend').unbind('webkitTransitionEnd');
-}
+        if (index == 1) {
+            $('.bk_toptips').show();
+            $('.bk_toptips span').html("敬请期待!");
+            setTimeout(function () {
+                $('.bk_toptips').hide();
+            }, 2000);
+        } else if (index == 2) {
+            location.href = '/category';
+        } else if (index == 3) {
+            location.href = '/cart';
+        } else if (index == 4) {
+            location.href = '/order_list';
+        }
+    }
 
-function onMenuItemClick(index) {
-  var mask = $('#mask');
-  var weuiActionsheet = $('#weui_actionsheet');
-  hideActionSheet(weuiActionsheet, mask);
-  if(index == 1) {
-
-  } else if(index == 2) {
-
-  } else if(index == 3){
-
-  } else {
-    $('.bk_toptips').show();
-    $('.bk_toptips span').html("敬请期待!");
-    setTimeout(function() {$('.bk_toptips').hide();}, 2000);
-  }
-}
+    // 将标题栏和标题保持一致
+    $('.bk_title_content').html(document.title);
 </script>
 
 @yield('my-js')
