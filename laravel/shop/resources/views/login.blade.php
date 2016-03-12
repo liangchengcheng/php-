157efcc7ff0c6ca16b9a5a16ca4c64f160ed8bd1
liangchengcheng <1038127753@qq.com>
2016-03-12 22:04:30 +0800

    修改了登录界面
    修改了登录界面

--- laravel/shop/resources/views/login.blade.php
+++ laravel/shop/resources/views/login.blade.php
@@ -25,7 +25,7 @@
                 <input class="weui_input" type="number" placeholder="请输入验证码"/>
             </div>
             <div class="weui_cell_ft">
-                <img src="/service/validate_code/create" class="bk_validate_code"/>
+                <img src="/validate_code/create" class="bk_validate_code"/>
             </div>
         </div>
     </div>
@@ -38,14 +38,66 @@
 
 @section('my-js')
     <script type="text/javascript">
+        //重新生成验证码
         $('.bk_validate_code').click(function () {
             $(this).attr('src', '/service/validate_code/create?random=' + Math.random());
         });
 
-        function onRegisterClick() {
-            username = $('input[name=username]').val();
-            password = $('input[name=passwd_phone]').val();
-            validate_code = $('input[name=validate_code]').val();
+        function onLoginClick() {
+            // 帐号 对账号进行基本的判断
+            var username = $('input[name=username]').val();
+            if(username.length == 0) {
+                $('.bk_toptips').show();
+                $('.bk_toptips span').html('帐号不能为空');
+                setTimeout(function() {$('.bk_toptips').hide();}, 2000);
+                return;
+            }
+
+            //对手机号进行基本的判断
+            if(username.indexOf('@') == -1) {
+                if(username.length != 11 || username[0] != 1) {
+                    $('.bk_toptips').show();
+                    $('.bk_toptips span').html('帐号格式不对!');
+                    setTimeout(function() {$('.bk_toptips').hide();}, 2000);
+                    return;
+                }
+            } else {
+                if(username.indexOf('.') == -1) {
+                    $('.bk_toptips').show();
+                    $('.bk_toptips span').html('帐号格式不对!');
+                    setTimeout(function() {$('.bk_toptips').hide();}, 2000);
+                    return;
+                }
+            }
+            // 密码 对密码进行基本判断
+            var password = $('input[name=password]').val();
+            if(password.length == 0) {
+                $('.bk_toptips').show();
+                $('.bk_toptips span').html('密码不能为空!');
+                setTimeout(function() {$('.bk_toptips').hide();}, 2000);
+                return;
+            }
+            if(password.length < 6) {
+                $('.bk_toptips').show();
+                $('.bk_toptips span').html('密码不能少于6位!');
+                setTimeout(function() {$('.bk_toptips').hide();}, 2000);
+                return;
+            }
+            // 验证码 对验证码进行基本的判断
+            var validate_code = $('input[name=validate_code]').val();
+            if(validate_code.length == 0) {
+                $('.bk_toptips').show();
+                $('.bk_toptips span').html('验证码不能为空!');
+                setTimeout(function() {$('.bk_toptips').hide();}, 2000);
+                return;
+            }
+            if(validate_code.length < 4) {
+                $('.bk_toptips').show();
+                $('.bk_toptips span').html('验证码不能少于4位!');
+                setTimeout(function() {$('.bk_toptips').hide();}, 2000);
+                return;
+            }
+            //发送ajax请求,post方式,地址,类型,参数,成功返回
             $.ajax({
                 type: "POST",
                 url: '/service/login',
@@ -56,6 +108,7 @@
                 },
                 success: function (data) {
                     if (data == null) {
+                        //返回的数据是空的话
                         $('.bk_toptips').show();
                         $('.bk_toptips span').html('服务端错误');
                         setTimeout(function () {
@@ -64,6 +117,7 @@
                         return;
                     }
                     if (data.status != 0) {
+                        //有数据的话
                         $('.bk_toptips').show();
                         $('.bk_toptips span').html(data.message);
                         setTimeout(function () {
@@ -77,9 +131,10 @@
                     setTimeout(function () {
                         $('.bk_toptips').hide();
                     }, 2000);
-                    //
+                    //登录成功的话就直接去category对应的界面
                     location.href = "/category";
                 },
+
                 error: function (xhr, status, error) {
                     console.log(xhr);
                     console.log(status);
