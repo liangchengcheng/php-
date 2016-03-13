9532f446a518983ccd68efa7ef8588f3fdc5ee80
liangchengcheng <1038127753@qq.com>
2016-03-14 07:55:29 +0800

    购物车
    购物车

--- laravel/shop/app/Http/Controllers/View/CartController.php
+++ laravel/shop/app/Http/Controllers/View/CartController.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Log;
 class CartController extends Controller
 {
 
-    //进去书籍类别的界面
+    /**
+     * 去购物车的界面
+     * @param Request $request
+     */
     public function toCart(Request $request)
     {
         //初始化一个数组
@@ -25,10 +28,24 @@ class CartController extends Controller
 
         $bk_cart_arr = ($bk_cart != null ? explode(",", $bk_cart) : array());
 
-        $member = $request->session() - get('member', '');
+        $member = $request->session()->get('member', '');
         if ($member != '') {
-            //$cart_items=$this->
+            $cart_items = $this->syncCart($member->id, $bk_cart_arr);
+            return response()->view('cart', ['cart_items' => $cart_items])->withCookie('bk_cart', null);
         }
+
+        foreach ($bk_cart_arr as $key => $value) {
+            $index = strpos($value, ":");
+            $cart_item = new CartItem;
+            $cart_item->id = $key;
+            $cart_item->product_id = substr($value, 0, $index);
+            $cart_item->count = (int)substr($value, $index + 1);
+            $cart_item->product = Product::find($cart_item->product_id);
+            if ($cart_item->product != null) {
+                array_push($cart_items, $cart_item);
+            }
+        }
+        return view('cart') - with('cart_items', $cart_items);
     }
 
     /**
@@ -38,7 +55,7 @@ class CartController extends Controller
      */
     private function syncCart($member_id, $bk_cart_arr)
     {
-        //根据id查找数据
+        //根据用户的id查找对应的购物车数据
         $cart_items = CartItem::where('member_id', $member_id)->get();
         $cart_items_arr = array();
 
@@ -64,15 +81,22 @@ class CartController extends Controller
             }
 
             //不存在的话就存进来
-            if($exits==false){
-                $cart_item=new CartItem();
-                $cart_item->member_id=$member_id;
-                $cart_item->product_id=$product_id;
-                $cart_item->count=$count;
+            if ($exits == false) {
+                $cart_item = new CartItem();
+                $cart_item->member_id = $member_id;
+                $cart_item->product_id = $product_id;
+                $cart_item->count = $count;
                 $cart_item->save();
-                $cart_item->product=Product::find($cart_item->product_id);
-                array_push($cart_items_arr,$cart_item);
+                $cart_item->product = Product::find($cart_item->product_id);
+                array_push($cart_items_arr, $cart_item);
             }
         }
+
+        //给每个对象附加产品的对象便于显示
+        foreach ($cart_items as $cart_item) {
+            $cart_item->product = Product::find($cart_item->product_id);
+            array_push($cart_items_arr, $cart_item);
+        }
+        return $cart_items_arr;
     }
 }
