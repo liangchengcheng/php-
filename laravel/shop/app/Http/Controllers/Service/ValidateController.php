0f6dabf7aa65e50f95628a5794e506841b224ab6
liangchengcheng <1038127753@qq.com>
2016-03-18 09:33:48 +0800

    修改部分代码
    修改部分代码

--- laravel/shop/app/Http/Controllers/Service/ValidateController.php
+++ laravel/shop/app/Http/Controllers/Service/ValidateController.php
@@ -16,7 +16,7 @@ class ValidateController extends Controller
   public function create(Request $request)
   {
     $validateCode = new ValidateCode;
-    //$request->session()->put('validate_code', $validateCode->getCode());
+    $request->session()->put('validate_code', $validateCode->getCode());
     return $validateCode->doimg();
   }
 
