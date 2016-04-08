d0971cb5343c5c49cc427dbb6a8ea0c465035439
liangchengcheng <1038127753@qq.com>
2016-04-08 22:26:55 +0800

    稍微复杂的sql
    稍微复杂的sql

---
+++ laravel/my_laravele/app/Contracts/TestContract.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lcc
+ * Date: 16/4/8
+ * Time: 上午10:42
+ */
+namespace app\Contracts;
+
+interface TestContract{
+    public function callMe($controller);
+}
